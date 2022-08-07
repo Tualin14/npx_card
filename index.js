7b79a6f665d2103cc94703e16f6ea09352b5c94c
Tualin14 <t@dzaaaaaa.com>
2022-08-07 12:15:31 +0800

    update

--- index.js
+++ index.js
@@ -2,10 +2,20 @@
 
 import gradient from "gradient-string";
 
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const log = async (s, delay = 160, randomized = true) => {
+  for (const c of s) {
+    process.stdout.write(c);
+    await sleep((randomized ? Math.random() : 1) * delay);
+  }
+  process.stdout.write("\n");
+};
+
 console.clear();
 
-// http://patorjk.com/software/taag/
-// ANSI Shadow
 console.log(
   gradient.rainbow.multiline(`
   ████████╗██╗   ██╗ █████╗ ██╗     ██╗███╗   ██╗ ██╗██╗  ██╗
@@ -16,6 +26,7 @@ console.log(
      ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═╝     ╚═╝
 `)
 );
+
 console.log(
   gradient.vice(
     [
@@ -26,3 +37,9 @@ console.log(
     ].join("\n")
   )
 );
+
+log(`
+Hi, I'm Tualin14(dzaaaaaa).
+You can contact me via the link above.
+
+`);
